134a7150a30230578a8defc0c2db8e70060fa9e8
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2021-01-11 17:22:53 -0600

    adding publisher
    add channel
    add connection

---
+++ app/services/publisher.rb
@@ -0,0 +1,17 @@
+class Publisher
+  def self.publish(exchange, message = {})
+    #grab the fanout exchange
+    event = channel.fanout("blog.#{exchange}")
+    event.publish(message.to_json)
+  end
+
+  def self.channel
+    @channel ||= connection.create_channel
+  end
+
+  def self.connection
+    @connection ||= Bunny.new.tap do |connect|
+      connect.start
+    end
+  end
+end
